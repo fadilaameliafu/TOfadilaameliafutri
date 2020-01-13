4b1bf6bf9a761c0aa5a29d117b933af672d0db29
Fadila Amelia Futri <31961538+fadilaameliafu@users.noreply.github.com>
2020-01-13 15:25:42 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+//NUMBER1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nama;
+
+    cout << "Enter your name = ";
+    cin >> nama;
+
+    cout << "Hello, my name is " + nama ;
+ 
+}
